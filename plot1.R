855690baa00e0289892066a665c8dc549f8b197b
porsmbao <bao.xinwei1995@gmail.com>
2020-05-06 17:51:49 +0800

    version 1

---
+++ plot1.R
@@ -0,0 +1,15 @@
+#plot 1 Global Active Power
+
+setwd("C:/Users/Bao Xinwei/Desktop/R/exploratory")
+library(dplyr)
+df <- read.table("household_power_consumption.txt", sep=";", header=TRUE, stringsAsFactors = FALSE)
+dt <- tbl_df(df)
+dt <- mutate(dt, Date = as.Date(Date, "%d/%m/%Y"))
+dt1 <- filter(dt, Date == as.Date("2007-02-01") | Date == as.Date("2007-02-02"))
+dt1 <- mutate(dt1, Global_active_power = as.numeric(Global_active_power))
+hist(dt1$Global_active_power, 
+     col = "red", 
+     main = "Global Active Power", 
+     xlab = "Global Active Power (kilowatts)")
+dev.copy(png, file = "plot1.png")
+dev.off()
